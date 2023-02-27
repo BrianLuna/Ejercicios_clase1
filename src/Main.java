d003b7e497be89fbe056f482e6e544cbfdfc0bc5
Brian Damian Luna <brian.damian.luna@gmail.com>
2023-02-27 11:17:32 -0300

    Segundo commit

---
+++ src/Main.java
@@ -0,0 +1,76 @@
+public class Main {
+    public static void main(String[] args) {
+        /* Ejer1 a */
+
+//        int numeroInicio= 9;
+//        int numeroFin= 7;
+//        int numeroActual = numeroInicio;
+//        while(numeroActual<=numeroFin){
+//            System.out.println(numeroActual);
+//            numeroActual++;
+//        }
+
+        /* Ejer1 b */
+       /* int numeroInicio= 1;
+        int numeroFin= 12;
+        int numeroActual = numeroInicio;
+        while(numeroActual<=numeroFin) {
+            if ((numeroActual % 2) == 0) {
+                System.out.println(numeroActual);
+            }
+            numeroActual++;
+//            numeroActual = numeroActual + 1;
+        }*/
+        /* Ejer1 c */
+/*        int numeroInicio= 2;
+        int numeroFin= 12;
+        boolean soloPares = false;
+
+        while(numeroInicio <= numeroFin){
+
+            if (soloPares && ((numeroInicio) % 2 == 0)){
+                System.out.println(numeroInicio);
+            }
+
+            if (!soloPares && ((numeroInicio) % 2 == 1)){
+                System.out.println(numeroInicio);
+            }
+
+            numeroInicio++;
+        }*/
+
+//        if((numeroInicio % 2) == 0){
+//            System.out.println(numeroInicio);
+//        }
+
+//        while(numeroInicio<=numeroFin) {
+//            if(soloPares &&((numeroInicio % 2) == 0)){
+//                System.out.println(numeroInicio);
+//            }
+//        }
+
+        /* Ejer1 d */
+/*        int numeroInicio= 1;
+        int numeroFin= 12;
+        for(int numeroActual = numeroFin; numeroActual>=numeroInicio;numeroActual--){
+            System.out.println(numeroActual);
+        }*/
+
+        /* Ejercicio 2*/
+        int ingresosMax= 489083;
+        int vehiculosMax= 3;
+        int inmueblesMax= 3;
+
+        int ingresosUsuario= 190000;
+        int vehiculosUsuario= 2;
+        int inmueblesUsuario= 2;
+        boolean activosSocietarios = false;
+
+        if((ingresosUsuario>=ingresosMax) || (vehiculosUsuario>=vehiculosMax) || (inmueblesUsuario>=inmueblesMax) || activosSocietarios){
+            System.out.println("Pertenece al Segmento alto");
+        }
+        else{
+            System.out.println("No pertenece al Segmento alto");
+        }
+    }
+}
